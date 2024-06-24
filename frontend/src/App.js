27cfb50abb62319bab3e612d4dfa1afd44ce9c16
Tina Pham <tip226@lehigh.edu>
2024-06-24 17:58:33 -0400

    added file input

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -24,6 +24,15 @@ function App() {
             onChange={handleTextChange}
             className="shadow appearance-none border border-pink-300 rounded w-full py-2 px-3 text-pink-700 leading-tight focus:outline-none focus:shadow-outline focus:border-pink-500"
           />
+          <div className="mb-4">
+            <label className="block text-pink-700 text-sm font-bold mb-2" htmlFor="file">File input:</label>
+            <input
+              type="file"
+              id="file"
+              onChange={handleFileChange}
+              className="shadow appearance-none border border-pink-300 rounded w-full py-2 px-3 text-pink-700 leading-tight focus:outline-none focus:shadow-outline focus:border-pink-500"
+            />
+          </div>
         </div>
       </form>
     </div>
