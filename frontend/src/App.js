c6d50b325e40b3b3573c9ad9acffbc557894103c
Tina Pham <tip226@lehigh.edu>
2024-06-24 17:36:23 -0400

    made the outside box

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,6 +12,13 @@ function App() {
     e.preventDefault();
   };
 
+  return (
+    <div className="container mx-auto p-4 bg-pink-100">
+      <form onSubmit={handleSubmit} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 border-2 border-pink-300">
+          
+      </form>
+    </div>
+  );
 }
 
 export default App;
